93424f057afb42eff6758f56cea8057185be7e2e
jooyeon <victoai999@gmail.com>
2023-07-24 23:33:52 +0900

    7_24

---
+++ prj1/src/prj1/CharTest3.java
@@ -0,0 +1,22 @@
+package prj1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+
+public class CharTest3 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FileOutputStream fout = new FileOutputStream( "res/data-char2.txt" );
+	  	
+		//텍스트파일 편집기
+		//byte 값  코드값이 저장됨
+		 
+		fout.write('0');
+		fout.write( 97);
+		  
+	}
+
+}
