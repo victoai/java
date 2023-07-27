32b37f4e912f9f6d1226e4f9e14ddff342a8204f
jooyeon <victoai999@gmail.com>
2023-07-28 02:26:58 +0900

    7-28  배열요소 삭제

--- prj1/src/prj1/ArrayDelete3.java
+++ prj1/src/prj1/ArrayDelete3.java
@@ -4,19 +4,20 @@ import java.util.Arrays;
 
 public class ArrayDelete3 {
     public static void main(String[] args) {
-        int[] originalArray = {1, 2, 3, 4, 5};
-        int indexToRemove = 2; // 삭제할 요소의 인덱스 (0부터 시작)
+        int[] array = {1, 2, 3, 4, 5};
+        int index = 2; // 삭제할 요소의 인덱스 (0부터 시작)
 
         // 배열에서 indexToRemove 위치의 요소를 삭제
-        int[] newArray = removeElement(originalArray, indexToRemove);
+        int[] newArray = removeItem(array, index);
         // 결과 출력
-        System.out.println("원래 배열: " + Arrays.toString(originalArray));
+        System.out.println("원래 배열: " + Arrays.toString(array));
         System.out.println("삭제 후 배열: " + Arrays.toString(newArray));
     }
+    
 
     
     
-    public static int[] removeElement(int[] array, int index) {
+    public static int[] removeItem(int[] array, int index) {
         // 새로운 배열 생성
         int[] newArray = new int[array.length - 1];
         // 삭제할 요소 이전까지 복사
