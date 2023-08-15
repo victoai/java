3c600cbdda2a091f90b6e52b216dd0c362016817
jooyeon <victoai999@gmail.com>
2023-08-16 00:15:25 +0900

    자바 20일차

--- prj1/src/day20Ex/chatt/Client1.java
+++ prj1/src/day20Ex/chatt/Client1.java
@@ -4,11 +4,13 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client1 {
 
 	 Socket socket;// 소켓
+	
 	 DataInputStream dataInputStream;       // 입력용보조스트림
 	 DataOutputStream dataOutputStream;     // 출려용보조스트림
 	 
@@ -17,18 +19,62 @@ public class Client1 {
 	public Client1() {	    
 		
 		
+		// 
+		
+		try {
+			socket = new Socket("localhost", 6700);
+
+			dataInputStream = new DataInputStream( socket.getInputStream());
+			dataOutputStream = new DataOutputStream ( socket.getOutputStream());	
+			dataSend();
+			dataRecv();
+			
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	
+
+
 	private void dataRecv() {
- 
-		
+		try {
+			String msg= dataInputStream.readUTF();
+			System.out.println( msg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
+
+
+
+
+
 	private void dataSend() {
+		
+		Scanner sc = new Scanner( System.in);
+		
+	
+		try {
+			String sendMsg  = sc.nextLine();
+			dataOutputStream.writeUTF(sendMsg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		 
 	}
+
+
+
 
 	public static void main(String[] args) {		 
 		new Client1();
--- prj1/src/day20Ex/chatt/Server1.java
+++ prj1/src/day20Ex/chatt/Server1.java
@@ -10,27 +10,75 @@ import java.util.Scanner;
 public class Server1 {
 	ServerSocket serverSocket;   //서버용 소켓
 	Socket clientSocket;	    // 클라이언트와 통신할 소켓 
+
 	DataInputStream dataInputStream;
 	DataOutputStream dataOutputStream;	
 	
 	
 	 public Server1() { // 5000~9000 사이  사용
+		  
 		 
-		 
+		 try {
+			serverSocket = new ServerSocket(6700);
+			clientSocket =serverSocket.accept();
+			
+			dataInputStream = new DataInputStream(clientSocket.getInputStream());
+			dataOutputStream= new DataOutputStream(clientSocket.getOutputStream());
+			
+			
+			//받기
+			 dataRecv();
+			//보내기
+			 dataSend();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		 
 		 
 	 }
 	
 	 
+ 
+
+	 
+
+	private void dataRecv() {
+		try {
+			String msg= dataInputStream.readUTF();
+			System.out.println( msg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+
+
+
+
+	private void dataSend() {
+		
+		Scanner sc = new Scanner( System.in);
+		
 	
-	private void dataSend() {		 	
+		try {
+			String sendMsg  = sc.nextLine();
+			dataOutputStream.writeUTF(sendMsg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		 
 	}
- 
-	
-	private void dataRecv() {
-		 
-	} 
+
+
+
+
 
 
 	public static void main(String[] args) {
