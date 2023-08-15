8ee30db31b87c201c0719c77ad4628b0e0b406bb
jooyeon <victoai999@gmail.com>
2023-08-15 23:40:50 +0900

    자바 20일차 채팅

---
+++ prj1/src/day20Ex/chatt/Client1.java
@@ -0,0 +1,38 @@
+package day20Ex.chatt;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client1 {
+
+	 Socket socket;// 소켓
+	 DataInputStream dataInputStream;       // 입력용보조스트림
+	 DataOutputStream dataOutputStream;     // 출려용보조스트림
+	 
+	 
+	
+	public Client1() {	    
+		
+		
+	}
+	
+	
+	private void dataRecv() {
+ 
+		
+		
+	}
+
+	private void dataSend() {
+		 
+	}
+
+	public static void main(String[] args) {		 
+		new Client1();
+
+	}
+
+}
---
+++ prj1/src/day20Ex/chatt/Server1.java
@@ -0,0 +1,41 @@
+package day20Ex.chatt;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server1 {
+	ServerSocket serverSocket;   //서버용 소켓
+	Socket clientSocket;	    // 클라이언트와 통신할 소켓 
+	DataInputStream dataInputStream;
+	DataOutputStream dataOutputStream;	
+	
+	
+	 public Server1() { // 5000~9000 사이  사용
+		 
+		 
+		 
+		 
+	 }
+	
+	 
+	
+	private void dataSend() {		 	
+		 
+	}
+ 
+	
+	private void dataRecv() {
+		 
+	} 
+
+
+	public static void main(String[] args) {
+		 new Server1();
+
+	}
+
+}
