3ff8b2716f75e52eaebee8e912decc85e49f6c69
jooyeon <victoai999@gmail.com>
2023-10-03 23:24:47 +0900

    자바2 4일차

---
+++ prj1/src/java2/day4/변경이유리한코드/계산기/A.java
@@ -0,0 +1,11 @@
+package java2.day4.변경이유리한코드.계산기;
+
+public class A implements Calculator {
+
+	@Override
+	public int add(int su1, int su2) {
+		System.out.println(" A calculator");
+		return su1+ su2;
+	}
+
+}
---
+++ prj1/src/java2/day4/변경이유리한코드/계산기/Calculator.java
@@ -0,0 +1,7 @@
+package java2.day4.변경이유리한코드.계산기;
+
+public interface Calculator {
+	
+	int add( int su1, int su2);
+
+}
